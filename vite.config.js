2126ba13081c8b5866834b0e8bf961e27f4744ae
Ivan Falcon <ivan.falcon@whiz.pe>
2022-02-05 14:23:08 -0500

    add support web3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     events: require.resolve('events'),
+  },
+  alias: {
+    web3: path.resolve(__dirname, './node_modules/web3/dist/web3.min.js'),
   },
   plugins: [react()],
   define: {
